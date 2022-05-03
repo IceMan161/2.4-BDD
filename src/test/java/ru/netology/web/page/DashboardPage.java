348807920060140066bc0f4f15e07778650fc610
Artem Sukhanov <IceMan161@yandex.ru>
2022-05-03 15:19:03 +0300

    Selectors

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,34 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+//    private SelenideElement firstCard = $("[data-test-id = '92df3f1c-a033-48e6-8390-206f6b1f56c0']");
+    private SelenideElement firstButton = $("[data-test-id = '92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button__text");
+//    private SelenideElement secondCard = $("[data-test-id = '0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
+    private SelenideElement secondButton = $("[data-test-id = '0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button__text");
+
+    private ElementsCollection cards = $$(".list__item div");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+    public MoneyTransferPage validDashbord() {
+
+    }
+
+}
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,4 +1,50 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+        $("[data-test-id = '92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button__text").click();
+        $("[data-test-id = 'amount'] .input__control").setValue("1000");
+        $("[data-test-id = 'from'] .input__control").setValue("5559 0000 0000 0002");
+        $("[data-test-id = 'action-transfer'] .button__text").click();
+
+
+    }
+
+    @Test
+
+    void mustTransferToSecondCard() {
+
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+        $("[data-test-id = '0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button__text").click();
+        $("[data-test-id = 'amount'] .input__control").setValue("1000");
+        $("[data-test-id = 'from'] .input__control").setValue("5559 0000 0000 0001");
+        $("[data-test-id = 'action-transfer'] .button__text").click();
+
+    }
+
 }
