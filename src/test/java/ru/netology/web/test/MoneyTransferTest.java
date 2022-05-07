64ecf3c1c8e9cae9f85991bdd9b0d032052829e6
Artem Sukhanov <IceMan161@yandex.ru>
2022-05-07 15:32:17 +0300

    mod1

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web.test;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPage;
@@ -10,11 +11,16 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class MoneyTransferTest {
 
-    @Test
-    void shouldTransferMoneyBetweenOwnCards() {
-
-        Configuration.holdBrowserOpen = true;
+    @BeforeEach
+    void shouldSet() {
+        Configuration.headless = true;
         open("http://localhost:9999");
+    }
+
+    @Test
+    void mustTransferToFirstCard() {
+
+//        Configuration.holdBrowserOpen = true;
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
 
@@ -24,6 +30,7 @@ public class MoneyTransferTest {
         var moneyTransferPage = dashboardPage.validCardFirst();
         var cardsInfo = DataHelper.getCardsInfo();
         var transferFirst = moneyTransferPage.transferAmountFirstCard(cardsInfo);
+        transferFirst.getCardBalance("[data-test-id = '92df3f1c-a033-48e6-8390-206f6b1f56c0']");
 
     }
 
@@ -31,8 +38,7 @@ public class MoneyTransferTest {
 
     void mustTransferToSecondCard() {
 
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
+//        Configuration.holdBrowserOpen = true;
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
@@ -41,7 +47,7 @@ public class MoneyTransferTest {
         var moneyTransferPage = dashboardPage.validCardSecond();
         var cardsInfo = DataHelper.getCardsInfo();
         var transferSecond = moneyTransferPage.transferAmountSecondCard(cardsInfo);
-
+        transferSecond.getCardBalance("[data-test-id = '0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
     }
 
 }
